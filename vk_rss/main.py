4f1ab2366e2e101e147b095a751e4d3c41451a1e
Dmitry Konishchev <konishchev@gmail.com>
2013-04-13 12:17:08 +0400

    Using tornado web server to handle HTTP requests

--- vk_rss/main.py
+++ vk_rss/main.py
@@ -4,23 +4,40 @@
 
 import logging
 import os
-import pprint
 import sys
+
+import tornado.ioloop
+import tornado.web
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
 import pycl.log
 
 from vk_rss import config
+from vk_rss import rss
 from vk_rss import tools
 
 LOG = logging.getLogger("vk-rss.main")
 
 
+class RootHandler(tornado.web.RequestHandler):
+    """The server's root handler."""
+
+    def get(self):
+        # TODO
+        self.set_header("Content-Type", "application/xml")
+        newsfeed = tools.get_newsfeed()
+        self.write(rss.generate(newsfeed))
+        #import pprint
+        #self.write(pprint.pformat(newsfeed))
+
+
 def main():
     """The script"s main function."""
 
+    assert hasattr(config, "PORT")
     assert hasattr(config, "ACCESS_TOKEN")
+
     if not hasattr(config, "DEBUG_MODE"):
         config.DEBUG_MODE = False
     if not hasattr(config, "WRITE_DEBUG"):
@@ -28,8 +45,12 @@ def main():
 
     pycl.log.setup(debug_mode = True)
 
-    newsfeed = tools.get_newsfeed()
-    #pprint.pprint(newsfeed)
+    application = tornado.web.Application([
+        ("/", RootHandler),
+    ], debug=config.DEBUG_MODE)
+
+    application.listen(config.PORT)
+    tornado.ioloop.IOLoop.instance().start()
 
 
 if __name__ == "__main__":
