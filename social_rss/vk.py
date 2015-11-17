4c30b01572b3893d874a569e7ee22694342ee6be
Dmitry Konishchev <konishchev@gmail.com>
2015-11-17 21:49:02 +0300

    Don't generate feed items for every movie added by someone

--- social_rss/vk.py
+++ social_rss/vk.py
@@ -97,8 +97,8 @@ def _get_newsfeed(access_token):
                     item = _post_item(users, user, api_item)
                 elif api_item["type"] in ("photo", "photo_tag"):
                     item = _photo_item(users, user, api_item)
-                elif api_item["type"] == "audio":
-                    continue # Don't generate items for every song added by someone
+                elif api_item["type"] == ("audio", "video"):
+                    continue # Don't generate items for every song or movie added by someone
                 elif api_item["type"] == "wall_photo":
                     continue # It duplicates post items with any photo
                 elif api_item["type"] == "friend":
